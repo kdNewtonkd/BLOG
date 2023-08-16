01f12c2a693d7dbeef9d377b6c1b031490c9cfa4
KDivalien <k.divalien@gmail.com>
2023-08-16 11:44:09 +0200

    init

--- core/blog/models.py
+++ core/blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-	name=models.CharField(max_length=20)
+	name=models.CharField(max_length=100)
 
 	def __str__(self):
 		return self.name
